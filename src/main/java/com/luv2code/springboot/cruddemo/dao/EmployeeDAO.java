af80bb71587a2d1232ffb1b2b08a95b479adf1a6
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-07 09:18:07 +0700

    spring boot get a employee

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -7,5 +7,13 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 public interface EmployeeDAO {
 
 	public List<Employee> findAll();
-		
+
+	public Employee findById(int id);
+
+	public void save(Employee employee);
+
+	public void deleteById(int id);
+
+
+
 }
--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,34 +1,44 @@
 package com.luv2code.springboot.cruddemo.rest;
 
-import java.util.List;
-
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
-import com.luv2code.springboot.cruddemo.entity.Employee;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-	private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-	// quick and dirty: inject employee dao (use constructor injection)
-	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
-	}
-	
-	// expose "/employees" and return list of employees
-	@GetMapping("/employees")
-	public List<Employee> findAll() {
-		return employeeDAO.findAll();
-	}
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
-	
+    // expose "/employees" and return list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null){
+            throw new RuntimeException(("Employee is not found - ")+employeeId);
+        }
+        return employee;
+    }
+
 }
 
 
